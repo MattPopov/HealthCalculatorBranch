c87f4094f14a1df0a92232c88407cae23f79e7c0
lataf <alexfatkin@gmail.com>
2024-02-11 12:38:28 +0300

    resp clear

--- resp.py
+++ resp.py
@@ -1,31 +1,37 @@
 best = 'отлично'
 good = 'хорошо'
-udovl = 'удовлетворительно'
+satisfactory = 'удовлетворительно'
 bad = 'плохо'
+
+
 def respiratory():
-    gender = input('Введите ваш пол: ')
+    gender = input('Введите ваш пол М или Ж: ')
+
     def proba(gender):
-        time=int(input('Введите время задержки воздуха на вдохе в секундах: '))
-        if gender=='Мужской' or gender=='мужской':
-            if time<35:
-                rproba=bad
-            elif time>=35 and time<=45:
-                rproba=udovl
-            elif time>45 and time<=60:
-                rproba=good
+        time = int(input('Введите время задержки воздуха на вдохе в секундах: '))
+        if gender == 'М':
+            if time < 35:
+                resp_probe = bad
+            elif 35 <= time <= 45:
+                resp_probe = satisfactory
+            elif 45 < time <= 60:
+                resp_probe = good
             else:
-                rproba=best
-        elif gender=='Женский' or gender=='женский':
-            if time<29:
-                rproba=bad
-            elif time>=29 and time<=40:
-                rproba=udovl
-            elif time>40 and time<=55:
-                rproba=good
+                resp_probe = best
+        elif gender == 'Ж':
+            if time < 29:
+                resp_probe = bad
+            elif 29 <= time <= 40:
+                resp_probe = satisfactory
+            elif 40 < time <= 55:
+                resp_probe = good
             else:
-                rproba=best
-        print('Ваш результат:',rproba)
+                resp_probe = best
+        print('Ваш результат:', resp_probe)
+
     proba(gender)
+
+
 print('Cделайте 2-3 глубоких вдоха и выдоха, а затем, сделав полный вдох, задержите дыхание')
 print('По окончании времени, нажмите на любую кнопку')
 input()
